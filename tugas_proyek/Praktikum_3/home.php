bc8a8956638774dc4cf43ce2507a0a8475776563
ilham Maulana Hakim <92973827+ilham12334@users.noreply.github.com>
2022-04-22 22:18:58 +0700

    Add files via upload

---
+++ tugas_proyek/Praktikum_3/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAS-STTNF</title>
+</head>
+<body>
+    <?php 
+        include_once ('atas.php');
+    ?>
+    <h1>Welcome Ilham Maulana Hakim !!!</h1>
+    <?php 
+        include_once ('bawah.php');
+    ?>
+</body>
+</html>
